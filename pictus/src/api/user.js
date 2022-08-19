8c613e7f865f4c31f2e9a598e1722ba18d6a6c50
yyeonzu <tooma22@naver.com>
2022-08-19 16:03:42 +0900

    feat fake API

---
+++ pictus/src/api/user.js
@@ -0,0 +1,24 @@
+// import axios from 'axios';
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import { Login } from '../App';
+
+// const LoginAPI = async (email, password) => {
+//   const navigate = useNavigate();
+//   return axios
+//     .post('https://jain5379.pythonanywhere.com/users/login/', {
+//       username: email,
+//       password: password,
+//     })
+//     .then((response) => {
+//       // console.log(response);
+//       alert('성공');
+//       navigate('/mainpage');
+//     })
+//     .catch((error) => {
+//       // console.log(response);
+//       alert('실패');
+//     });
+// };
+
+// export default LoginAPI;
